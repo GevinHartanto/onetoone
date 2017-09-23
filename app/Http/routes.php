b21914d6a012829ee0f2dd59f74c7a35ff1fe2f4
GevinHartanto <gevinhartanto@gmail.com>
2017-09-23 11:38:18 +0700

    Reading and Deleting Data

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -33,3 +33,39 @@ Route::get('/update', function(){
 	$address->name = "Jl. Pendek Sekali, langsung nyampe";
 	$address->save();
 });
+
+Route::get('/read', function(){
+	$user = User::findOrFail(1);
+	echo $user->name;
+	echo $user->address->name;
+});
+
+Route::get('/delete', function(){
+	$user = User::findOrFail(1);
+	$user->address()->delete();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
